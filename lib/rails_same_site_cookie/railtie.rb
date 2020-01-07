4cad62fb7d859b6a40e4e9f2f7e3e08b68a1c6fd
Cristian Planas <gawyntrakand@gmail.com>
2020-01-07 15:57:37 -0800

    Adding RailsSameSiteCookie::Middleware after Rack::Runtime

--- lib/rails_same_site_cookie/railtie.rb
+++ lib/rails_same_site_cookie/railtie.rb
@@ -3,7 +3,7 @@ require 'rails_same_site_cookie/middleware'
 module RailsSameSiteCookie
   class Railtie < Rails::Railtie
     initializer "rails_same_site_cookie.setup_middleware" do
-      Rails.application.middleware.insert_after ActionDispatch::Static, Middleware
+      Rails.application.middleware.insert_after ::Rack::Runtime, ::RailsSameSiteCookie::Middleware
     end
   end
 end
