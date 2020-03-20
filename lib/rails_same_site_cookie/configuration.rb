bcac63149f4f62e0ac2cc2bd8e47a18172021f0b
Marcos Teixeira <m.viniteixeira@gmail.com>
2020-03-20 13:41:42 +0000

    Adding option to generate a legacy cookie for browsers that dont support same site cookies yet. https://web.dev/samesite-cookie-recipes/#handling-incompatible-clients

--- lib/rails_same_site_cookie/configuration.rb
+++ lib/rails_same_site_cookie/configuration.rb
@@ -1,9 +1,10 @@
 module RailsSameSiteCookie
   class Configuration
-    attr_accessor :user_agent_regex
+    attr_accessor :user_agent_regex, :generate_legacy_cookie
 
     def initialize
       @user_agent_regex = nil
+      @generate_legacy_cookie = false
     end
   end
 end
--- lib/rails_same_site_cookie/middleware.rb
+++ lib/rails_same_site_cookie/middleware.rb
@@ -13,10 +13,11 @@ module RailsSameSiteCookie
       status, headers, body = @app.call(env)
 
       regex = RailsSameSiteCookie.configuration.user_agent_regex
+      generate_legacy = RailsSameSiteCookie.configuration.generate_legacy_cookie
       set_cookie = headers['Set-Cookie']
       if (regex.nil? or regex.match(env['HTTP_USER_AGENT'])) and not (set_cookie.nil? or set_cookie.strip == '')
         parser = UserAgentChecker.new(env['HTTP_USER_AGENT'])
-        if parser.send_same_site_none?
+        if parser.send_same_site_none? || generate_legacy
           cookies = set_cookie.split(COOKIE_SEPARATOR)
           ssl = Rack::Request.new(env).ssl?
 
@@ -26,10 +27,17 @@ module RailsSameSiteCookie
               cookie << '; Secure'
             end
 
-            unless cookie =~ /;\s*samesite=/i
-              cookie << '; SameSite=None'
+            if parser.send_same_site_none?
+              unless cookie =~ /;\s*samesite=/i
+                cookie << '; SameSite=None'
+              end
             end
 
+            if generate_legacy
+              cookie_name, cookie_value = cookie.split('=', 2)
+              legacy_cookie = "#{COOKIE_SEPARATOR} #{cookie_name}-legacy=#{cookie_value}"
+              cookie << legacy_cookie
+            end
           end
 
           headers['Set-Cookie'] = cookies.join(COOKIE_SEPARATOR)
